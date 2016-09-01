47312eb146f53881a5901eb6b757140076250538
rchikhi <rayan.chikhi@univ-lille1.fr>
2016-09-01 17:44:26 +0200

    testing multi-os multi-compiler travis

--- example/run-tiny.sh
+++ example/run-tiny.sh
@@ -1,1 +1,2 @@
-../build/bcalm -in tiny_read.fa -kmer-size 12 -abundance-min 1 $1 $2
+bcalm=`find ../*/bcalm | head -n 1` # covers bin/ and build/ cases
+$bcalm -in tiny_read.fa -kmer-size 12 -abundance-min 1 $1 $2
