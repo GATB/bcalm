1d179b9d56cf227ec1305fc58a1876a6a8281a69
rchikhi <rayan.chikhi@univ-lille1.fr>
2017-06-10 00:18:22 +0200

    updated tiny script

--- example/run-tiny.sh
+++ example/run-tiny.sh
@@ -1,2 +1,2 @@
 bcalm=`find ../*/bcalm | head -n 1` # covers bin/ and build/ cases
-$bcalm -in tiny_read.fa -kmer-size 12 -abundance-min 1 $1 $2
+$bcalm -in tiny_read.fa -kmer-size 13 -abundance-min 1 $1 $2
